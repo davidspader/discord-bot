99012ce17069a3c7496c878b92d9f777c7974383
David Spader Ambrozio <davidspader97@gmail.com>
2020-12-21 09:25:48 -0300

    creating a standard embed

---
+++ commands/embed.js
@@ -0,0 +1,23 @@
+const discord = require('discord.js');
+
+const name = 'embed';
+const help = 'bla bla bla bla embed';
+
+const execute = (client, msg, args) => {
+    const embed = new discord.MessageEmbed()
+        .setColor('#f2a30b')
+        .setTitle(process.env.PREFIX + name)
+        .setURL('https://discord.js.org/')
+        .setAuthor(client.user.username, client.user.displayAvatarURL())
+        .setDescription('**bla bla bla bla embed**\nbla bla bla bla embed\nbla bla bla bla embed\nbla bla bla bla embed\n')
+        .setTimestamp()
+        .setFooter(client.user.username, client.user.displayAvatarURL());
+
+    msg.reply(embed);
+};
+
+module.exports = {
+    name,
+    help,
+    execute,
+};
